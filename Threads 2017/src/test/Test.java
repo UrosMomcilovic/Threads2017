15de41844873a43adfc8ad22a369fd2187d50e3d
Vladan <Vladan@192.168.0.23>
2017-05-28 03:52:46 +0200

    Intro - simple wait

---
+++ Threads 2017/src/test/Test.java
@@ -0,0 +1,38 @@
+package test;
+
+public class Test {
+	
+	public void simpleDelay() {
+		System.out.println("Wait 2s...");
+		long l1 = System.currentTimeMillis();
+		while (System.currentTimeMillis() < l1 + 2000) {
+		}
+		System.out.println("Done.");
+	}
+	
+	public synchronized void simpleWait() {
+		System.out.println("Wait 2s...");
+		try {
+			wait(2000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println("Done.");
+	}
+	
+	public synchronized void loopWait() {
+		System.out.println("Count from 1 to 5...");
+		for (int j = 0; j < 5; j++) {
+			try {
+				wait(1000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			System.out.println(j + 1);
+		}
+		System.out.println("Done.");
+	}
+
+}
